bc1e83b70a1ef0810217c8e8c30449b34f3691da
Hasanjon <hasanjonsadullayev97@gmail.com>
2022-03-02 22:28:26 +0500

    OS module haqida tushunchalar

---
+++ OS/OS-modul.js
@@ -0,0 +1,25 @@
+const os = require("os");
+
+// os => Operating System
+
+
+// Platform
+// console.log(os.platform());  // win32
+
+// Architect
+// console.log(os.arch()); // System type: x64
+
+// Inform
+// console.log(os.cpus()); // CP haqida to'liq informatsia
+
+// Free memory
+// console.log(os.freemem()); // CP dagi hotira(joy)
+
+// Nechta joyqoldi
+// console.log(os.totalmem()); //
+
+// Entry directory
+// console.log(os.homedir()); // C:Users/hasan
+
+// CP qancha vaxt ishlagani
+// console.log(os.uptime()); // shu hozirgi vaxtgacha kampyuter qancha vaxt ishlagani
